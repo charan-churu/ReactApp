16cb599494b0d7d82db3bc59017464c3df1f06dc
charan-churu <92526595+charan-churu@users.noreply.github.com>
2023-12-30 19:45:01 +0530

    Add files via upload
    This App stores the input data in a temporary array and moves each element to the final array for a particular time.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import App1 from './components/App1';
+function App() {
+  return (
+    <div className="App">
+      <App1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/App1.jsx
@@ -0,0 +1,85 @@
+
+import React, { useState, useEffect } from 'react';
+import './App1.css'
+const App1 = () => {
+  const [inputText, setInputText] = useState('');
+  const [queue, setQueue] = useState([]);
+  const [displayedText, setDisplayedText] = useState([]);
+  const [showSuccessPopup, setShowSuccessPopup] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (queue.length > 0) {
+        setDisplayedText(prevDisplayedText => [...prevDisplayedText, queue[0]]);
+        setQueue(queue.slice(1));
+      }
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, [queue]);
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setQueue([...queue, inputText]);
+    setInputText('');
+  };
+
+  const handleEndClick = () => {
+    if (queue.length === 0) {
+      setShowSuccessPopup(true);
+      alert(`Success! The queue is empty.`)
+    } else {
+      const checkQueueEmpty = setInterval(() => {
+        if (queue.length === 0) {
+          setShowSuccessPopup(true);
+          alert(`Success! The queue is empty.`)
+          clearInterval(checkQueueEmpty);
+        }
+      }, 1000);
+    }
+  };
+
+  const handleReset = () => {
+    setInputText('');
+    setQueue([]);
+    setDisplayedText([]);
+    setShowSuccessPopup(false);
+  };
+
+  return (
+    <div className='body'>
+      <h1 className='h1'>React Queue App</h1>
+      <div className='inputText'>
+           <label alt='enter the input text:'>Enter the input text:</label>
+          <input type="text" value={inputText} onChange={handleInputChange} className='input' />
+          <button onClick={handleSubmit} className='Add'>Add</button>
+        </div>
+      <div className='position'>
+        
+        
+        <div className='tempData'>
+          <h2>Temp Data:</h2>
+          <ul>
+            {queue.map((item, index) => <li key={index}>{item}</li>)}
+          </ul>
+        </div>
+        </div>
+
+        <div className='finalData'>
+          <h2>Final Data:</h2>
+          <ul>
+            {displayedText.map((item, index) => <li key={index}>{item}</li>)}
+          </ul>
+         </div> 
+        <div className='buttons'>
+            <button onClick={handleEndClick} className='End'>End</button>
+            <button onClick={handleReset} className='Reset'>Reset</button>
+          </div>
+          {showSuccessPopup}
+      </div>
+      );
+}
+export default App1;
